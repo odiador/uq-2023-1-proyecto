94d984edf302dc2e641ad75a5c1f73cec1dba207
FabianValencia23 <127154183+FabianValencia23@users.noreply.github.com>
2023-03-26 17:38:47 -0500

    Add javaDoc
    Add javaDoc in the class Cliente

--- ws_concesionarioCarros/src/co/edu/uniquindio/concesionarioCarros/model/Cliente.java
+++ ws_concesionarioCarros/src/co/edu/uniquindio/concesionarioCarros/model/Cliente.java
@@ -2,21 +2,23 @@ package co.edu.uniquindio.concesionarioCarros.model;
 
 public class Cliente extends Usuario implements CompraVentaVehiculo {
 
+    /**
+     * Este el constructor de la clase
+     */
     public Cliente() {
     }
 
     @Override
     public void venderVehiculo() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'venderVehiculo'");
     }
 
     @Override
     public void comprarVehiculo() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'comprarVehiculo'");
     }
 
+    /**
+     * Se encarga de alquilar el vehiculo
+     */
     public void alquilarVehiculo() {
 
     }

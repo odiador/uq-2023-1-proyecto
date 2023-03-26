1805855d04633b48451753d9f4ce3d6c6bfcc146
FabianValencia23 <127154183+FabianValencia23@users.noreply.github.com>
2023-03-26 14:25:27 -0500

    Add javaDoc
    Add javaDoc in the class AutoMovil

--- ws_concesionarioCarros/src/co/edu/uniquindio/concesionarioCarros/model/Automovil.java
+++ ws_concesionarioCarros/src/co/edu/uniquindio/concesionarioCarros/model/Automovil.java
@@ -1,33 +1,68 @@
 package co.edu.uniquindio.concesionarioCarros.model;
 
 public class Automovil extends Vehiculo {
+
+    //ATRIBUTOS
     private Integer numeroPasajeros;
     private Integer numeroBolsasAire;
     private Integer numeroPuertas;
 
+    /**
+     * El constructor de la clase 
+     */
     public Automovil() {
     }
 
+    /**
+     * Obtiene el numero de pasajeros
+     * 
+     * @return numeroPasajeros
+     */
     public Integer getNumeroPasajeros() {
         return numeroPasajeros;
     }
 
+    /**
+     * Cambia el numero de pasajeros
+     * 
+     * @param numeroPasajeros
+     */
     public void setNumeroPasajeros(Integer numeroPasajeros) {
         this.numeroPasajeros = numeroPasajeros;
     }
 
+    /**
+     * Obiene el numero de bolsas de aire
+     * 
+     * @return numeroBolsasAire
+     */
     public Integer getNumeroBolsasAire() {
         return numeroBolsasAire;
     }
 
+    /**
+     * Cambia el numero de bolsas de aire
+     * 
+     * @param numeroBolsasAire
+     */
     public void setNumeroBolsasAire(Integer numeroBolsasAire) {
         this.numeroBolsasAire = numeroBolsasAire;
     }
 
+    /**
+     * Obtiene el numero de puertas
+     * 
+     * @return numeroPuertas
+     */
     public Integer getNumeroPuertas() {
         return numeroPuertas;
     }
 
+    /**
+     * Cambia el numero de puertas
+     * 
+     * @param numeroPuertas
+     */
     public void setNumeroPuertas(Integer numeroPuertas) {
         this.numeroPuertas = numeroPuertas;
     }

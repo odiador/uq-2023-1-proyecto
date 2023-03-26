f584272ccd6795f54cb6ed0aaf6d0be525f191e3
FabianValencia23 <127154183+FabianValencia23@users.noreply.github.com>
2023-03-26 15:55:52 -0500

    Add javaDoc
    Add javaDoc in the class Bus

--- ws_concesionarioCarros/src/co/edu/uniquindio/concesionarioCarros/model/Bus.java
+++ ws_concesionarioCarros/src/co/edu/uniquindio/concesionarioCarros/model/Bus.java
@@ -1,24 +1,49 @@
 package co.edu.uniquindio.concesionarioCarros.model;
 
 public class Bus extends Van {
+    //ATRIBUTOS
     private Integer numeroEjes;
     private Integer numeroSalidasEmergencia;
 
+    /**
+     * Este es el constructor de la clase
+     * 
+     */
     public Bus() {
     }
 
+    /**
+     * Obtiene el numero de ejes
+     * 
+     * @return numeroEjes
+     */
     public Integer getNumeroEjes() {
         return numeroEjes;
     }
 
+    /**
+     * Cambia el numero de ejes
+     * 
+     * @param numeroEjes
+     */
     public void setNumeroEjes(Integer numeroEjes) {
         this.numeroEjes = numeroEjes;
     }
 
+    /**
+     * Obtiene el numero de salidas de emergencia
+     * 
+     * @return numeroSalidasEmergencia
+     */
     public Integer getNumeroSalidasEmergencia() {
         return numeroSalidasEmergencia;
     }
 
+    /**
+     * Cambia el numero de salidas de emergencia
+     * 
+     * @param numeroSalidasEmergencia
+     */
     public void setNumeroSalidasEmergencia(Integer numeroSalidasEmergencia) {
         this.numeroSalidasEmergencia = numeroSalidasEmergencia;
     }

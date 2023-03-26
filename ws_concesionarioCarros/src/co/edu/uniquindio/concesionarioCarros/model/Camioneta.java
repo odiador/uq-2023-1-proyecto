4e63c8ecd85938e368ef2ac814ac01f2a2fb3536
FabianValencia23 <127154183+FabianValencia23@users.noreply.github.com>
2023-03-26 17:13:17 -0500

    Add javaDoc
    Add javaDoc in the class Camioneta

--- ws_concesionarioCarros/src/co/edu/uniquindio/concesionarioCarros/model/Camioneta.java
+++ ws_concesionarioCarros/src/co/edu/uniquindio/concesionarioCarros/model/Camioneta.java
@@ -1,15 +1,30 @@
 package co.edu.uniquindio.concesionarioCarros.model;
 
 public class Camioneta extends Sedan {
+    
+    //ATRIBUTOS
     private Boolean es4x4;
 
+    /** 
+     * Cntr etodo constructor de la clase
+     */
     public Camioneta() {
-    }
-
+    }     
+    
+    /**
+     * Obtiene si la camioneta es 4x4 o no
+     * 
+     * @return
+     */
     public Boolean getEs4x4() {
         return es4x4;
     }
 
+    /**
+     * Obtiene si la camioneta es 4x4 o no
+     * 
+     * @param
+     */
     public void setEs4x4(Boolean es4x4) {
         this.es4x4 = es4x4;
     }

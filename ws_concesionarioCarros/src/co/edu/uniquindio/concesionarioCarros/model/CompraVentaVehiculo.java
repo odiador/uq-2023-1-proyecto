fe4a4f30c3e3545b4efbaa1eccc21051e993bb2a
FabianValencia23 <127154183+FabianValencia23@users.noreply.github.com>
2023-03-26 17:37:53 -0500

    Add javaDoc
    Add javaDoc in the Interface CompraVentaVehiculo

--- ws_concesionarioCarros/src/co/edu/uniquindio/concesionarioCarros/model/CompraVentaVehiculo.java
+++ ws_concesionarioCarros/src/co/edu/uniquindio/concesionarioCarros/model/CompraVentaVehiculo.java
@@ -2,6 +2,13 @@ package co.edu.uniquindio.concesionarioCarros.model;
 
 public interface CompraVentaVehiculo {
 
+    /**
+     * Permite verder un vehiculo
+     */
     void venderVehiculo();
+
+    /**
+     *  Permite comprar un vehiculo
+     */
     void comprarVehiculo();
 }

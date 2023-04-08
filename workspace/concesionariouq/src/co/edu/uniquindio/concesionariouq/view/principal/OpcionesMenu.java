82d804f0520adaaeb5f196e7e74e64f5a218c976
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-07 22:39:06 -0500

    Update OpcionesMenu.java
    Added new options for the options

--- workspace/concesionariouq/src/co/edu/uniquindio/concesionariouq/view/principal/OpcionesMenu.java
+++ workspace/concesionariouq/src/co/edu/uniquindio/concesionariouq/view/principal/OpcionesMenu.java
@@ -1,7 +1,20 @@
 package co.edu.uniquindio.concesionariouq.view.principal;
 
 public enum OpcionesMenu {
-	AGREGAR_VEHICULO("Agregar Vehiculo"), AGREGAR_CLIENTE("Agregar Cliente");
+	AGREGAR_VEHICULO("Agregar Vehiculo"), 
+	AGREGAR_CLIENTE("Agregar Cliente"), 
+	COMPRAR_VEHICULO("Comprar Vehiculo"),
+	ALQUILAR_VEHICULO("Alquilar Vehiculo"), 
+	ELIMINAR_CLIENTE("Eliminar cliente"), 
+	ELIMINAR_VEHICULO("Eliminar Vehiculo"), 
+	ACTUALIZAR_VEHICULO("Actualizar Vehiculo"),
+	ACTUALIZAR_CLIENTE("Actualizar Vliente"), 
+	VENDER_VEHICULO("Vender Vehiculo"), 
+	GENERAR_REPORTE("Generar Reporte"), 
+	ACTUALIZAR_INFO("Actualizar Info"),
+	CAMBIAR_CONTRASENA("Cambiar Contraseña"), 
+	ACERCA_DE("Acerca de");
+
 	private String text;
 
 	private OpcionesMenu(String text) {
@@ -14,5 +27,14 @@ public enum OpcionesMenu {
 
 	public void setText(String text) {
 		this.text = text;
+	}
+
+	public static OpcionesMenu obtenerValorOpcion(String text) {
+		OpcionesMenu[] arr = OpcionesMenu.values();
+		for (OpcionesMenu opcionesMenu : arr)
+			if (opcionesMenu.getText().equals(text))
+				return opcionesMenu;
+
+		return null;
 	}
 }

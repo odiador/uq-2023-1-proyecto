4f8417a895e4eac8d99883ee5ea8c1d16ece196c
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-07 23:04:28 -0500

    Update Main.java
    Co-Authored-By: FabianValencia23 <127154183+FabianValencia23@users.noreply.github.com>
    Co-Authored-By: ElJuanchito <99854835+ElJuanchito@users.noreply.github.com>

--- workspace/concesionariouq/src/application/Main.java
+++ workspace/concesionariouq/src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
-import co.edu.uniquindio.concesionariouq.view.login.EscenaLogin;
+import co.edu.uniquindio.concesionariouq.model.Cliente;
+import co.edu.uniquindio.concesionariouq.view.principal.EscenaPrincipal;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,7 +10,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Scene scene = new EscenaLogin(primaryStage, 400, 400);
+//			Scene scene = new EscenaLogin(primaryStage, 440, 440);
+			Scene scene = new EscenaPrincipal(primaryStage, new Cliente("", "", "", ""));
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();

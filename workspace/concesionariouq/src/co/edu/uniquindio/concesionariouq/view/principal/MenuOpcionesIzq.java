53693a2f311094fa97fa3972948c29a19c477d9d
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-07 23:03:35 -0500

    Update MenuOpcionesIzq.java
    Added handler for a button presioned
    
    Co-Authored-By: FabianValencia23 <127154183+FabianValencia23@users.noreply.github.com>
    Co-Authored-By: ElJuanchito <99854835+ElJuanchito@users.noreply.github.com>

--- workspace/concesionariouq/src/co/edu/uniquindio/concesionariouq/view/principal/MenuOpcionesIzq.java
+++ workspace/concesionariouq/src/co/edu/uniquindio/concesionariouq/view/principal/MenuOpcionesIzq.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.concesionariouq.view.principal;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class MenuOpcionesIzq extends VBox {
@@ -8,10 +9,19 @@ public class MenuOpcionesIzq extends VBox {
 
 	public MenuOpcionesIzq(MenuComunicationListener comunicacion) {
 		this.comunicacion = comunicacion;
+		initComp();
 	}
 
 	private void initComp() {
-
+		OpcionesMenu[] opciones = OpcionesMenu.values();
+		for (int i = 0; i < opciones.length; i++) {
+			Label boton = new Label(opciones[i].getText());
+			boton.setId("boton-menu");
+			this.getChildren().add(boton);
+			boton.setOnMouseReleased(evento -> {
+				comunicacion.botonIzqPresionado(((Label) evento.getSource()).getText());
+			});
+		}
 	}
 
 }

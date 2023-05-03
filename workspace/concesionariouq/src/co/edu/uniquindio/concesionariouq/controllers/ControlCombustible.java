ed5362600ed9365134cc5498eb1c05ba2f50eb75
Juancho <perdomocardenas18@gmail.com>
2023-05-03 09:49:09 -0500

    Hanciendo cambios en panel de combustible

--- workspace/concesionariouq/src/co/edu/uniquindio/concesionariouq/controllers/ControlCombustible.java
+++ workspace/concesionariouq/src/co/edu/uniquindio/concesionariouq/controllers/ControlCombustible.java
@@ -2,12 +2,20 @@ package co.edu.uniquindio.concesionariouq.controllers;
 
 import co.edu.uniquindio.concesionariouq.exceptions.CampoException;
 import co.edu.uniquindio.concesionariouq.model.Combustible;
+import co.edu.uniquindio.concesionariouq.util.Boton;
 import co.edu.uniquindio.concesionariouq.util.Utility;
 import co.edu.uniquindio.concesionariouq.view.agregarVehiculo.PanelConVolver;
 import co.edu.uniquindio.concesionariouq.view.menu.PanelCombustible;
+import co.edu.uniquindio.concesionariouq.view.menu.PanelElectrico;
+import co.edu.uniquindio.concesionariouq.view.menu.PanelHibrido;
 import co.edu.uniquindio.concesionariouq.view.menu.TipoCombustible;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.stage.Stage;
 
 public class ControlCombustible {
 	public static void irACambiarCombustible(PanelConVolver panel, Combustible combustible,
@@ -29,7 +37,56 @@ public class ControlCombustible {
 		try {
 			actualizarValorThrows(panelCombustible, valorNuevo);
 		} catch (CampoException e) {
-			 
+
 		}
+	}
+
+	public static void irCombustibles(PanelConVolver panel, String valorCombustible, EventHandler<? super MouseEvent> eventoVolver) {
+		TipoCombustible tipoCombustible = TipoCombustible.getTipo(valorCombustible);
+		PanelConVolver panelConVolver = null;
+
+		switch (tipoCombustible) {
+		case ELECTRICO:
+			panelConVolver = new PanelElectrico();
+			break;
+		case HIBRIDO:
+			panelConVolver = new PanelHibrido();
+			break;
+		case DIESEL:
+			
+			break;
+		case GASOLINA:
+
+			break;
+
+		default:
+			break;
+		}
+
+		((Stage) panel.getScene().getWindow())
+				.setTitle("Agregar " + valorCombustible + " | Concesionario UQ");
+
+		HBox hbox = new HBox();
+		ScrollPane scrollPane = new ScrollPane(panelConVolver);
+		scrollPane.setId("centered-box");
+		scrollPane.setBorder(null);
+		scrollPane.setFitToHeight(true);
+		scrollPane.setFitToWidth(true);
+
+		Boton botonVolver = new Boton("Volver", eventoVolver);
+		Boton botonAgregar = new Boton("Agregar", panelConVolver);
+
+		hbox.getChildren().add(botonVolver);
+		hbox.getChildren().add(botonAgregar);
+
+		Insets insets = new Insets(10, 10, 10, 10);
+		HBox.setMargin(botonAgregar, insets);
+		HBox.setMargin(botonVolver, insets);
+
+		HBox.setHgrow(botonVolver, Priority.ALWAYS);
+		HBox.setHgrow(botonAgregar, Priority.ALWAYS);
+
+		panel.setCenter(scrollPane);
+		panel.setBottom(hbox);
 	}
 }

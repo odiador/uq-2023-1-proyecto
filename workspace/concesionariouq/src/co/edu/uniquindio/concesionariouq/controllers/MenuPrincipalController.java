72e57f55f6e92cc9cde61354c14027f7b6548cd3
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-05-20 22:47:00 -0500

    Updated menu principal

---
+++ workspace/concesionariouq/src/co/edu/uniquindio/concesionariouq/controllers/MenuPrincipalController.java
@@ -0,0 +1,134 @@
+package co.edu.uniquindio.concesionariouq.controllers;
+
+import co.edu.uniquindio.concesionariouq.util.FxUtility;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.util.Duration;
+
+public class MenuPrincipalController {
+	@FXML
+	private ImageView imgLogo;
+
+	@FXML
+	private ImageView imgUsuario;
+
+	@FXML
+	private ImageView imgMenuBarra;
+
+	@FXML
+	private BorderPane menuPane;
+
+	@FXML
+	private SplitPane mainPane;
+
+	@FXML
+	private BorderPane contentPane;
+
+	@FXML
+	private Label labelNombre;
+
+	private boolean barraBloqueada;
+
+	@FXML
+	void initialize() {
+		imgLogo.fitWidthProperty().bind(menuPane.widthProperty().multiply(0.8));
+		imgUsuario.fitHeightProperty().bind(menuPane.widthProperty().subtract(15));
+	}
+
+	@FXML
+	void extenderMenuEvent(MouseEvent event) {
+		extenderMenuAction();
+	}
+
+	private void extenderMenuAction() {
+		if (!barraBloqueada) {
+			cambiarMenuTam(1);
+			labelNombre.setVisible(true);
+		}
+	}
+
+	private void cambiarMenuTam(int num) {
+		mainPane.getDividers().forEach(divider -> {
+			KeyValue kv = new KeyValue(divider.positionProperty(), divider.getPosition() + num);
+			KeyFrame kf = new KeyFrame(Duration.millis(1000), kv);
+			Timeline timeline = new Timeline(kf);
+			timeline.play();
+		});
+	}
+
+	@FXML
+	void contraerMenuEvent(MouseEvent event) {
+		contraerMenuAction();
+	}
+
+	private void contraerMenuAction() {
+		if (!barraBloqueada) {
+			cambiarMenuTam(-1);
+			labelNombre.setVisible(false);
+		}
+	}
+
+	@FXML
+	void agregarMenuEvent(ActionEvent event) {
+
+	}
+
+	@FXML
+	void eliminarEvent(ActionEvent event) {
+
+	}
+
+	@FXML
+	void venderEvent(ActionEvent event) {
+
+	}
+
+	@FXML
+	void comprarEvent(ActionEvent event) {
+
+	}
+
+	@FXML
+	void alquilarEvent(ActionEvent event) {
+
+	}
+
+	@FXML
+	void reportesEvent(ActionEvent event) {
+
+	}
+
+	@FXML
+	void bloquearBarraEvent(ActionEvent event) {
+		ButtonType btnExtendido = new ButtonType("Extendido");
+		ButtonType btnContraido = new ButtonType("Contraido");
+		ButtonType btnNo = new ButtonType("Quitar");
+		ButtonType botonAlerta = FxUtility.crearDecisionAlerta("Decision", "Que tipo de bloqueo quieres tener",
+				"Eligue el tipo de bloqueo que deseas para la barra lateral de la ventana", AlertType.INFORMATION, 600,
+				btnExtendido, btnContraido, btnNo);
+		barraBloqueada = false;
+		if (botonAlerta == btnExtendido) {
+			extenderMenuAction();
+			imgMenuBarra.setImage(new Image("/resources/images/Candado Bloqueado.png"));
+			barraBloqueada = true;
+		} else if (botonAlerta == btnContraido) {
+			contraerMenuAction();
+			imgMenuBarra.setImage(new Image("/resources/images/Candado Bloqueado.png"));
+			barraBloqueada = true;
+		} else {
+			imgMenuBarra.setImage(new Image("/resources/images/Candado Libre.png"));
+			contraerMenuAction();
+		}
+	}
+}
---
+++ workspace/concesionariouq/src/co/edu/uniquindio/concesionariouq/util/FxUtility.java
@@ -0,0 +1,112 @@
+package co.edu.uniquindio.concesionariouq.util;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Side;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class FxUtility {
+	public static void setAsNumberTextfield(TextField tf) {
+		tf.textProperty().addListener(new ChangeListener<String>() {
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				if (!newValue.matches("\\d*")) {
+					tf.setText(newValue.replaceAll("[^\\d]", ""));
+					abrirContextMenu(tf, "Este campo solo puede tener numeros");
+				}
+			}
+
+		});
+	}
+
+	private static void abrirContextMenu(Node nodo, String msg) {
+		final ContextMenu menu = new ContextMenu();
+		menu.getItems().add(new MenuItem(msg));
+		menu.show(nodo, Side.BOTTOM, 0, 0);
+
+		Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), evento -> {
+			menu.hide();
+		}));
+		timeline.play();
+	}
+
+	public static void setAsIntegerTextfield(TextField tf) {
+		tf.textProperty().addListener(new ChangeListener<String>() {
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				if (newValue.isEmpty())
+					return;
+				try {
+					Integer.parseInt(tf.getText());
+				} catch (Exception e) {
+					observable.removeListener(this);
+					tf.setText(oldValue);
+					observable.addListener(this);
+					abrirContextMenu(tf, "Este campo solo puede tener numeros enteros");
+				}
+			}
+		});
+	}
+
+	public static void setAsIntegerTextfield(TextField tf, int minValue, int maxValue) {
+		tf.textProperty().addListener(new ChangeListener<String>() {
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				if (newValue.isEmpty())
+					return;
+				try {
+					int num = Integer.parseInt(tf.getText());
+					if (num >= minValue && num <= maxValue)
+						return;
+				} catch (Exception e) {
+				}
+				observable.removeListener(this);
+				tf.setText(oldValue);
+				observable.addListener(this);
+				abrirContextMenu(tf, "Este campo solo puede tener numeros entre " + minValue + " y " + maxValue);
+			}
+		});
+	}
+
+	public static void cambiarTituloStage(Parent parent, String title) {
+		parent.sceneProperty().addListener(((observableScene, oldScene, newScene) -> {
+			parent.getScene().windowProperty().addListener((observableWindow, oldWindow, newWindow) -> {
+				Stage stage = (Stage) parent.getScene().getWindow();
+				stage.setTitle(title);
+			});
+		}));
+
+	}
+
+	public static ButtonType crearDecisionAlerta(String titulo, String header, String contenido, AlertType alertType,
+			double width, ButtonType... buttonTypes) {
+		Alert alerta = new Alert(alertType, contenido, buttonTypes);
+		alerta.setTitle(titulo);
+		alerta.setHeaderText(header);
+		alerta.setWidth(width);
+		return alerta.showAndWait().orElse(null);
+	}
+
+	public static void mostrarMensaje(String titulo, String header, String contenido, AlertType alertType) {
+		Alert alert = new Alert(alertType);
+		alert.setTitle(titulo);
+		alert.setHeaderText(header);
+		alert.setContentText(contenido);
+		alert.show();
+	}
+
+	public static void generarAdvertenciaSobreescritura(Runnable metodo) {
+		ButtonType boton = crearDecisionAlerta("Decision", "Sobreescritura de campos",
+				"Advertencia de sobreescritura de campos", AlertType.INFORMATION, 700, ButtonType.OK, ButtonType.CLOSE);
+		if (boton == ButtonType.OK)
+			metodo.run();
+	}
+}

15412ec49f5a01a8154bba2886e60bf7154f20ce
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-05-23 17:34:25 -0500

    Added new custom exceptions

---
+++ workspace/concesionariouq/src/co/edu/uniquindio/concesionariouq/exceptions/NullException.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio.concesionariouq.exceptions;
+
+public class NullException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/***
+	 * Es el constructor de la clase {@link NullException}
+	 * 
+	 * @param msg
+	 */
+	public NullException(String msg) {
+		super(msg);
+	}
+}
---
+++ workspace/concesionariouq/src/co/edu/uniquindio/concesionariouq/exceptions/UsuarioEncontradoException.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio.concesionariouq.exceptions;
+
+public class UsuarioEncontradoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Es el constructor de la clase {@link UsuarioEncontradoException}
+	 * 
+	 * @param msg
+	 */
+	public UsuarioEncontradoException(String msg) {
+		super(msg);
+	}
+}

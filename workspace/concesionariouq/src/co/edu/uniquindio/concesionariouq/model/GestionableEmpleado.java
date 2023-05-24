f694288e8a5cbf24cc477bf71c9c8567dc08588e
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-05-23 19:51:02 -0500

    Added a new interface

---
+++ workspace/concesionariouq/src/co/edu/uniquindio/concesionariouq/model/GestionableEmpleado.java
@@ -0,0 +1,23 @@
+package co.edu.uniquindio.concesionariouq.model;
+
+import java.util.List;
+
+import co.edu.uniquindio.concesionariouq.exceptions.AtributosFaltantesException;
+import co.edu.uniquindio.concesionariouq.exceptions.NullException;
+import co.edu.uniquindio.concesionariouq.exceptions.UsuarioEncontradoException;
+import co.edu.uniquindio.concesionariouq.exceptions.UsuarioNoEncontradoException;
+
+public interface GestionableEmpleado {
+
+	public void agregarEmpleado(Empleado empleado)
+			throws UsuarioEncontradoException, AtributosFaltantesException, NullException;
+
+	public void eliminarEmpleado(String id)
+			throws UsuarioNoEncontradoException, NullException;
+
+	public Empleado buscarEmpleado(String id);
+
+	public boolean validarEmpleado(String id);
+
+	public List<Empleado> listarEmpleados();
+}

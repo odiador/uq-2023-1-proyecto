79c1d9ddaff53effad23c5ce91aff0272fa1d1f0
ElJuanchito <perdomocardenas18@gmail.com>
2023-05-29 23:50:14 -0500

    Agregando nuevos parametros a la clase Alquiler

--- workspace/concesionariouq/src/co/edu/uniquindio/concesionariouq/model/Alquiler.java
+++ workspace/concesionariouq/src/co/edu/uniquindio/concesionariouq/model/Alquiler.java
@@ -1,15 +1,48 @@
 package co.edu.uniquindio.concesionariouq.model;
 
-public class Alquiler extends Transaccion{
+import java.time.LocalDate;
+
+public class Alquiler extends Transaccion {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+	private LocalDate fechaAlquiler;
+	private LocalDate fechaLimite;
+
+	/**
+	 * Constructor con parametros.
+	 * @param codigo
+	 * @param vehiculo
+	 * @param fechaAlquiler
+	 * @param fechaLimite
+	 */
+	public Alquiler(String codigo, Vehiculo vehiculo, LocalDate fechaLimite) {
+		super(codigo, vehiculo);
+		this.fechaAlquiler = LocalDate.now();
+		this.fechaLimite = fechaLimite;
+	}
+
 	@Override
 	public TipoTransaccion getTipoTransaccion() {
 		return TipoTransaccion.ALQUILER;
+	}
+
+	public LocalDate getFechaAlquiler() {
+		return fechaAlquiler;
+	}
+
+	public void setFechaAlquiler(LocalDate fechaAlquiler) {
+		this.fechaAlquiler = fechaAlquiler;
+	}
+
+	public LocalDate getFechaLimite() {
+		return fechaLimite;
+	}
+
+	public void setFechaLimite(LocalDate fechaLimite) {
+		this.fechaLimite = fechaLimite;
 	}
 
 }
